bd78c8d7993682ba226ed18c63670ec809d9520f
Nuryana <otgnuryana@gmail.com>
2020-11-03 15:47:31 +0700

    menambahkan status

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
 
 <body>
     <h1>Hello Kawan!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis consequatur quas inventore at, unde laudantium dolore ea placeat magnam repudiandae pariatur reprehenderit molestiae voluptates fuga. Ad recusandae aut blanditiis? Consequuntur.</p>
 </body>
 
 </html>
